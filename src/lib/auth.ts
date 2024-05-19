d2ba3dc12908d14a989d884b873aea7aeb0c2072
Saysan777 <alokaryal32@gmail.com>
2024-05-19 11:52:20 +0545

    resolve 2

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -23,6 +23,7 @@ export const authConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   callbacks: {
